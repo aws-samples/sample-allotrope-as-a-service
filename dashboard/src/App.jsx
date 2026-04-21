a563a13bf9843e365aff2e7c2fb674b0caf0f7e5
Duverney Tavares <duverney@amazon.com>
2026-04-21 12:00:44 -0400

    Add Apache 2.0 license headers to all source files, add LICENSE.md

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -1,3 +1,8 @@
+// Copyright 2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// Licensed under the Apache License, Version 2.0 (the "License").
+// You may not use this file except in compliance with the License.
+// A copy of the License is located at http://aws.amazon.com/apache2.0/
+// This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND.
 import { useState, useEffect } from 'react'
 import AppLayout from '@cloudscape-design/components/app-layout'
 import TopNavigation from '@cloudscape-design/components/top-navigation'
